9148f09bef902c53e64f69fac95bf0aa11f5c0fb
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-06-06 11:37:03 -0400

    more info in task list

--- src/components/teamTasks/taskListSection/TeamTaskItem.js
+++ src/components/teamTasks/taskListSection/TeamTaskItem.js
@@ -12,6 +12,25 @@ class TeamTaskItem extends React.Component {
     this.props.selectTaskDetail(event, this.props.t, this.props.pages, this.props.tags, this.props.users)
   }
 
+  renderTagList = () => {
+    if (!!this.props.tags) {
+      return this.props.tags.map((tag) => {
+        if (!!tag.title) {
+          return <span className="ind-tag">
+            <i className="material-icons" key = {tag.title} style={{fontSize: "12px", top:"3px"}}>label</i> {tag.title}</span>
+          }
+        })
+      } else {
+        return <span className="ind-tag no-display"></span>
+      }
+    }
+    formattedSavedDate = (dateArg) => {
+      let dateSaved = new Date(dateArg)
+      let timeSaved =  new Date(dateArg).toLocaleTimeString()
+      console.log(timeSaved)
+      return `${dateSaved.toLocaleDateString()} at ${timeSaved}`
+    }
+
   render() {
     if (!this.props.t) {
       return <div>Loading..</div>
@@ -23,6 +42,10 @@ class TeamTaskItem extends React.Component {
         <div className="task-list-item-title">
           {this.props.t.title}
         </div>
+
+
+        {this.renderTagList()}
+          <br /><span className="list-updated">Updated {this.formattedSavedDate(this.props.t.updated_at)}</span>
 
 
 
